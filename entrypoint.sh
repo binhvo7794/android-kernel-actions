c2de727fa720b9994c40eccf18692f611c8932c2
binhvo7794 <66165345+binhvo7794@users.noreply.github.com>
2024-07-20 01:10:59 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -209,7 +209,7 @@ if ! make O=out $arch_opts $make_opts $host_make_opts "$defconfig"; then
 fi
 msg "Begin building kernel..."
 
-make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)" Image.gz dtbo prepare
+make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)" Image.gz dtbo.img prepare
 
 if ! make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)"; then
     err "Failed building kernel, probably the toolchain is not compatible with the kernel, or kernel source problem"
